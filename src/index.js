89d7aeb0057692abaa059ee957941653649215a7
Dhaya3132 <dhayalanu103@gmail.com>
2024-03-26 17:10:09 +0530

    connecting to firebase

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs} from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCPx5W45ueJ5BFEIHHq7lNhrGnY2BGKzVA",
+
     authDomain: "chat-room-c7c1d.firebaseapp.com",
     projectId: "chat-room-c7c1d",
     storageBucket: "chat-room-c7c1d.appspot.com",
