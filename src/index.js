1e6134cb5db31e0faf0e41252d82382103f01369
Dhaya3132 <dhayalanu103@gmail.com>
2024-03-27 08:08:35 +0530

    adding to fire

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import { initializeApp } from 'firebase/app';
 
-import { getFirestore, collection, getDocs} from 'firebase/firestore';
+import { getFirestore, collection, getDocs, addDoc} from 'firebase/firestore';
 
 const firebaseConfig = {
-
-    authDomain: "chat-room-c7c1d.firebaseapp.com",
-    projectId: "chat-room-c7c1d",
-    storageBucket: "chat-room-c7c1d.appspot.com",
-    messagingSenderId: "65973620512",
-    appId: "1:65973620512:web:509e39468a4e7943875145",
-    measurementId: "G-E84C8Y5HZ3"
+    
   };
 
 initializeApp(firebaseConfig);
@@ -24,12 +18,40 @@ getDocs(collectionref).then(
         let chatroom =[]
         snapshot.docs.forEach(
             (doc)=> 
-            chatroom.push({...doc.data(), id:doc.id})
+            chatroom.push({...doc.data()})
         )
-        console.log(chatroom);
+        // console.log(chatroom);
     }
 ).catch(
     (err)=>{ console.log(err);
 });
  
 
+//Chatroom class
+
+class Chatroom{
+    constructor(room, username){
+      this.room = room;
+      this.username = username;
+      this.chats = collectionref;
+    }
+    async addchat(message)
+    {
+        // const now = new Date();
+        const chat = {
+            message,
+            room: this.room,
+            username: this.username
+            // created_at: firebase.firestore.Timestamp.fromDate(now)
+        };
+
+
+        const response_data = addDoc(this.chats, chat);
+        return response_data;
+    }
+}
+  
+  const chatrooom = new Chatroom('general','dhayaa');
+//   console.log(chatrooom);
+
+  chatrooom.addchat('hello everyone').then(()=> console.log('chat addedd'));
